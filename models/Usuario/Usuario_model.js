eed5105bd3c1864e9e0f3a2f9403c79cca29d7ea
xanjaca <cruzjona354@gmail.com>
2022-12-16 08:38:56 -0800

    Controladores + modelo + dbconexion

--- models/Usuario/Usuario_model.js
+++ models/Usuario/Usuario_model.js
@@ -1,35 +1,90 @@
-const sql = require("../db.js");
+const Sequelize = require('sequelize');
+const sequelize = new Sequelize('sqlite::memory:');
 
-// constructor, nos conectamos al tabla de Usuarios desde nuestra base de datos.
-var Usuario = sql.extend({
-    tableName: "usuario",
-});
-
-usuario = new Usuario();
-
-Usuario.getAll = result => {
-    usuario.find('all', function (err, rows) {
-        if (err) {
-            console.log("error: ", err);
-            result(null, err);
-            return;
-        }
-        console.log("usuarios: ", rows);
-        result(null, rows);
+module.exports = (sequelize, type) => {
+    return sequelize.define('usuario', {
+        ID: {
+            type: Sequelize.INTEGER,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        Nombre: {
+            type: Sequelize.STRING,
+            required: 'El nombre es obligatorio',
+            allowNull: false,
+        },
+        Apellido_1: {
+            type: Sequelize.STRING,
+            allowNull: false,
+        },
+        Apellido_2: {
+            type: Sequelize.STRING,
+            allowNull: false,
+        },
+        Email: {
+            type: Sequelize.STRING,
+            allowNull: false,
+        },
+        Telefono: {
+            type: Sequelize.INTEGER,
+            allowNull: false,
+        },
+        Contrasenna: {
+            type: Sequelize.STRING,
+            allowNull: false,
+        },
+        Rol: {
+            type: Sequelize.INTEGER,
+            allowNull: false,
+        },
+    }, {
+        tableName: 'usuario',
+        createdAt:false,
+        updatedAt: false,
     });
 }
 
-Usuario.getById = (usuarioId, result) => {
-    usuario.find('first',{where : `ID = ${usuarioId}`}, function (err, rows) {
-        if (err) {
-            console.log("error: ", err);
-            result(null, err);
-            return;
-        }
-        console.log("usuarios: ", rows);
-        result(null, rows);
-    });
-}
 
 
-module.exports = Usuario;
+
+// const sql = require("../db.js");
+
+
+// // constructor, nos conectamos al tabla de Usuarios desde nuestra base de datos.
+// var Usuario = sql.extend({
+//     tableName: "usuario",
+// });
+
+// usuario = new Usuario();
+
+// Usuario.getAll = result => {
+//     usuario.find('all', function (err, rows) {
+//         if (err) {
+//             console.log("error: ", err);
+//             result(null, err);
+//             return;
+//         }
+//         console.log("usuarios: ", rows);
+//         result(null, rows);
+//     });
+// }
+
+// Usuario.getById = (usuarioId, result) => {
+//     usuario.find('first',{where : `ID = ${usuarioId}`}, function (err, rows) {
+//         if (err) {
+//             console.log("error: ", err);
+//             result(null, err);
+//             return;
+//         }
+//         console.log("usuarios: ", rows);
+//         result(null, rows);
+//     });
+// }
+
+
+// module.exports = Usuario;
+
+
+
+
+
--- models/db.js
+++ models/db.js
@@ -1,12 +1,26 @@
-const mysqlModel = require('mysql-model');
+// const mysqlModel = require('mysql-model');
+const Sequelize = require('sequelize');
+const UserModel = require('./Usuario/Usuario_model');
+
 /* Buscamos el archivo que habíamos creado antes. */
 const dbConfig = require("../config/db_config.js");
 
-var connection = mysqlModel.createConnection({
+const sequelize = new Sequelize(dbConfig.DB, dbConfig.USER, dbConfig.PASSWORD,{
     host: dbConfig.HOST,
-    user: dbConfig.USER,
-    password: dbConfig.PASSWORD,
-    database: dbConfig.DB
+    dialect: 'mysql',
 });
 
-module.exports = connection;
+const User = UserModel(sequelize, Sequelize);
+
+sequelize.sync().then(()=>{
+    console.log('Tablas sincronizadas');
+})
+
+// var connection = mysqlModel.createConnection({
+//     host: dbConfig.HOST,
+//     user: dbConfig.USER,
+//     password: dbConfig.PASSWORD,
+//     database: dbConfig.DB
+// });
+
+module.exports = User;
