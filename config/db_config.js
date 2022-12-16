7f2c9a7d62326c2a8fe822a54183681f58438251
xanjaca <cruzjona354@gmail.com>
2022-12-16 08:44:11 -0800

    Add port to db_config

--- config/db_config.js
+++ config/db_config.js
@@ -1,6 +1,7 @@
 module.exports = {
-    HOST: process.env.HOST || "localhost",
-    USER: process.env.USER || "root",
-    PASSWORD: process.env.PASSWORD || "",
-    DB: process.env.DB || "acdemycdb"
+    HOST: process.env.HOST || "mysql-100641-0.cloudclusters.net",
+    USER: process.env.USER || "acdemyccoffee",
+    PASSWORD: process.env.PASSWORD || "9XuHkWK9",
+    DB: process.env.DB || "acdemycdb",
+    PORT: process.env.PORT || "10014"
 };
