6a2a0ee8b3404098cbadb2c1937c937b10bfae47
xanjaca <cruzjona354@gmail.com>
2022-12-16 09:00:29 -0800

    Quit port

--- config/db_config.js
+++ config/db_config.js
@@ -1,7 +1,7 @@
 module.exports = {
     HOST: process.env.HOST || "mysql-100641-0.cloudclusters.net",
     USER: process.env.USER || "acdemyccoffee",
-    PASSWORD: process.env.PASSWORD || "9XuHkWK9",
+    PASSWORD: process.env.PASSWORD || "Jaca2001!",
     DB: process.env.DB || "acdemycdb",
     PORT: process.env.PORT || "10014"
 };
