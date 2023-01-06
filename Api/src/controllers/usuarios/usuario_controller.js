36599ea988f6d7606ad08a5bf164d6830a994c1c
xanjaca <cruzjona354@gmail.com>
2023-01-06 13:41:56 -0800

    UsuarioByCedula
    Implemente el endpoint para buscar el usuario por cedula.

--- Api/src/controllers/usuarios/usuario_controller.js
+++ Api/src/controllers/usuarios/usuario_controller.js
@@ -2,7 +2,41 @@ const { response, request } = require('express');
 const bcrypt = require('bcryptjs');
 const { Usuario } = require('../../config/modelsdb');
 
+//Get Usuario by Cedula
+
+const usuarioGetByCedula = async (req = request, res = response) => {
+    const { cedula } = req.params;
+    const usuario = await Usuario.findOne({
+        where: { cedula }, include: [{
+            model: Usuario,
+            attributes: ['nombre', 'apellidos', 'email', 'cedula', 'telefono', 'direccion', 'estado', 'fk_institucion'],
+            where: { estado: 1, fk_institucion: req.header('fk_institucion') },
+        },
+        {
+            model: TipoUsuarioxUsuario,
+            attributes: ['fk_tipo_usuario', 'fk_usuario']
+        }]
+    });
+    res.json({
+        usuario
+    });
+}
+
+
 const usuariosGet = async (req = request, res = response) => {
+    //Obtener el fk_institucion. que viene en el header
+    const fk_institucion = req.header('fk_institucion');
+    if (fk_institucion == null) {
+        return res.status(400).json({
+            msg: 'No se ha enviado el parametro fk_institucion en el header'
+        });
+    }
+    //Obtenemos el id que viene en el request
+    if (req.query.id) {
+        usuarioGetByCedula(req, res);
+        return
+    }
+
     //Buscar todos los usuarios
     const usuarios = await Usuario.findAll();
 
@@ -40,7 +74,7 @@ const usuariosDelete = async (req, res = response) => {
     //Eliminamos el usuario
     const usuario = await Usuario.findByPk(id);
     await usuario.destroy();
-    
+
     res.json({
         msg: "Se elimino correctamente el usuario",
         usuario
